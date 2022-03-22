762fb16c3caf5de157b12a6b4a2c024de7832d1d
eshanajain <82397896+eshanajain@users.noreply.github.com>
2022-03-22 20:20:50 +0530

    Add files via upload

---
+++ bed=room.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage(basket.jpg);
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd'. modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#ff0000");
+    text("Bedroom", 45, 75);
+    noFill();
+    stroke("#ff0000");
+    rect(30,60,450,350);
+}
+
+ function modelLoaded() {
+     console.log("Model Loaded!")
+     status = true;
+     objectDetector.detect(img. gotResult);
+ } 
+ 
+ function gotResult(error, results) {
+     if (error) {
+         console.log(error);
+     }
+     console.log(results);
+     }
+ 
